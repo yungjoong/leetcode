5c3f7754bc614f6e3accf10e745c67524360478f
yungjoong song <yungjoong.song@gmail.com>
2020-05-06 16:54:30 +0900

    Add 27, 28 Python Code

---
+++ 28.implement-str-str.py
@@ -0,0 +1,54 @@
+#
+# @lc app=leetcode id=28 lang=python3
+#
+# [28] Implement strStr()
+#
+# https://leetcode.com/problems/implement-strstr/description/
+#
+# algorithms
+# Easy (33.94%)
+# Likes:    1418
+# Dislikes: 1774
+# Total Accepted:    623.6K
+# Total Submissions: 1.8M
+# Testcase Example:  '"hello"\n"ll"'
+#
+# Implement strStr().
+# 
+# Return the index of the first occurrence of needle in haystack, or -1 if
+# needle is not part of haystack.
+# 
+# Example 1:
+# 
+# 
+# Input: haystack = "hello", needle = "ll"
+# Output: 2
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: haystack = "aaaaa", needle = "bba"
+# Output: -1
+# 
+# 
+# Clarification:
+# 
+# What should we return when needle is an empty string? This is a great
+# question to ask during an interview.
+# 
+# For the purpose of this problem, we will return 0 when needle is an empty
+# string. This is consistent to C's strstr() and Java's indexOf().
+# 
+#
+
+# @lc code=start
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if len(needle) == 0 :
+            return 0
+        else :
+            return haystack.find(needle)
+        
+# @lc code=end
+
